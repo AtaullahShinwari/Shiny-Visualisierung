82e37d48c4cd869b02d9a940be7966547e8ab3cc
RBTSDT <96663828+RBTSDT@users.noreply.github.com>
2021-12-26 14:29:01 +0100

    Add files via upload

---
+++ Shiny-Visualisierung/Stochastik_WS2122_Gruppe_I/app.R
@@ -0,0 +1,41 @@
+library(shiny)
+
+### Daten Laden ###
+
+
+### Quellen ### https://stackoverflow.com/questions/42047422/create-url-hyperlink-in-r-shiny
+urlcat <- a("Link", href="https://i.redd.it/eknbn2b019v71.png")
+
+
+ui <- fluidPage(
+    tabsetPanel(type = "tabs",
+                tabPanel("Startseite",
+                         sidebarPanel(
+                             h4("Stochastik Gruppe I"),
+                             p("Robert Seidentopf s0537217"),
+                             p("Ata"),
+                             p("Chris"),
+                             img(src = "HTW_logo_V2.png"),
+                             p( br(),
+                                tagList("URL Katze:", urlcat)
+                             )
+                         ),
+                         mainPanel(
+                             h1(strong(("Korrelation ist nicht gleich Kausalität."))), br(),
+                             img(src = "Katze_Korrelation.png"), br(),
+                             
+                         )),
+                tabPanel("Exploration"),
+                tabPanel("Satz von Bayes")
+                #Quelle: https://shiny.rstudio.com/articles/tabsets.html
+                
+                
+    ))
+
+
+
+server <- function(input, output) {
+
+}
+
+shinyApp(ui = ui, server = server)
