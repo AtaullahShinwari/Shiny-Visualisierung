c677c454c54b3130c7c4d9c6ac9ad68b839101ba
RBTSDT <dontcarebear@posteo.de>
2021-12-27 15:49:44 +0100

    Rohdatendarstellung aktualisiert.

--- Stochastik_WS2122_Gruppe_I/app.R
+++ Stochastik_WS2122_Gruppe_I/app.R
@@ -27,7 +27,7 @@ ui <- fluidPage(
                              img(src = "Katze_Korrelation.png"), br(),
                              
                          )),
-                tabPanel("Exploration", sidebarLayout(position = "left", fluid = TRUE,sidebarPanel(selectInput("plotname", "Plot auswählen", c("Altersverteilung",""))), mainPanel(plotlyOutput("selectplot")))),
+                tabPanel("Exploration", sidebarLayout(position = "left", fluid = TRUE,sidebarPanel(selectInput("plotname", "Plot auswählen", c("Altersverteilung","Art des Brustschmerzes","Ruhepulsverteilung", "Cholesterolspiegel","Blutzuckerspiegelverteilung","Ergebnisse des Ruhe-Elektrokardiogramms", "Höchster gemessener Herzschlag", "Angina durch Belastung", "OldPeak", "ST_Slope", "HeartDisease"))), mainPanel(plotlyOutput("selectplot")))),
                 tabPanel("Satz von Bayes")
                 #Quelle: https://shiny.rstudio.com/articles/tabsets.html
                 
@@ -37,20 +37,108 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-
-  reactivedf <- reactive({heart})
-  
+ 
   output$selectplot <- renderPlotly({   
     
     if(input$plotname == "Altersverteilung"){
       heart_age <- heart %>% group_by(Age, Sex) %>% count()
       Plot <- ggplotly(ggplot(heart_age, aes(x = Age, y = n)) 
                        + scale_y_continuous(name = "Anzahl") 
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3")) #, labels = c("M" = "männlich","F" = "weiblich")
                        + ggtitle("Altersverteilung") 
                        + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
-                       + geom_bar(aes(fill = Sex), stat = "identity", colour = "yellowgreen", position = "dodge", width = 0.7))
-      }
-    
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "Art des Brustschmerzes"){
+      heart_chestpaintype <- heart %>% group_by(ChestPainType, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_chestpaintype, aes(x = ChestPainType, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("Brustschmerz") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "Ruhepulsverteilung"){
+      heart_restingbp <- heart %>% group_by(RestingBP, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_restingbp, aes(x = RestingBP, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("Ruhepulsverteilung") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "Cholesterolspiegelverteilung"){
+      heart_cholesterol <- heart %>% group_by(Cholesterol, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_cholesterol, aes(x = Cholesterol, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("Cholesterolspiegelverteilung") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "Blutzuckerspiegelverteilung"){
+      heart_fastingbs <- heart %>% group_by(FastingBS, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_fastingbs, aes(x = FastingBS, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("Blutzuckerspiegelverteilung") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "Ergebnisse des Ruhe-Elektrokardiogramms"){
+      heart_restingecg <- heart %>% group_by(RestingECG, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_restingecg, aes(x = RestingECG, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("Ergebnisse des Ruhe-Elektrokardiogramms") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "Höchster gemessener Herzschlag"){
+      heart_maxhr <- heart %>% group_by(MaxHR, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_maxhr, aes(x = MaxHR, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("Höchster gemessener Herzschlag") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "Angina durch Belastung"){
+      heart_exerciseangina <- heart %>% group_by(ExerciseAngina, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_exerciseangina, aes(x = ExerciseAngina, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("Angina durch Belastung") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "OldPeak"){
+      heart_oldpeak <- heart %>% group_by(OldPeak, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_oldpeak, aes(x = OldPeak, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("OldPeak") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "ST_Slope"){
+      heart_st_slope <- heart %>% group_by(ST_Slope, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_st_slope, aes(x = ST_Slope, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("ST_Slope") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
+    if(input$plotname == "HeartDisease"){
+      heart_heartdisease <- heart %>% group_by(HeartDisease, Sex) %>% count()
+      Plot <- ggplotly(ggplot(heart_heartdisease, aes(x = HeartDisease, y = n)) 
+                       + scale_y_continuous(name = "Anzahl")
+                       + scale_fill_manual("Legende",values = c("M" = "chartreuse4","F" = "chartreuse3"))
+                       + ggtitle("HeartDisease") 
+                       + theme(plot.title = element_text(size = 11, hjust = 0.5)) 
+                       + geom_bar(aes(fill = Sex), stat = "identity", position = "dodge", width = 0.7))
+    }
     Plot
     })
   
